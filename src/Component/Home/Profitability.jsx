5b8b992881eb0923067895a7c547ad21c9df6374
SumiAkter2 <akteralisha36@gmail.com>
2023-11-16 18:40:43 +0600

    added emailjs

--- src/Component/Home/Profitability.jsx
+++ src/Component/Home/Profitability.jsx
@@ -1,21 +1,38 @@
+import { useRef } from "react";
 import PrimaryButton from "../Share/Button/PrimaryButton";
+import emailjs from "emailjs-com";
 
 const Profitability = () => {
+  const form = useRef();
+
   const submitEmail = (e) => {
     e.preventDefault();
-    const name1 = e.target.name.value;
-    const email1 = e.target.email.value;
-    const phone1 = e.target.phone.value;
-    console.log({ name: name1, email: email1, phone: phone1 });
-    const emailUseName = name1;
-    const emailBody = `Hi ,My name is ${name1}. I want to start free trial now.,My phone number is ${phone1} and email is ${email1}`;
 
-    const mailtoLink = `mailto:sumiakter6335@gmail.com?subject=${encodeURIComponent(
-      emailUseName
-    )}&body=${encodeURIComponent(emailBody)}`;
+    emailjs
+      .sendForm(
+        "service_5gks1fg",
+        "template_mq0hs6o",
+        form.current,
+        "zaDkgqaW77bTY4aEn"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    // console.log({ name: name1, email: email1, phone: phone1 });
+    // const emailUseName = name1;
+    // const emailBody = `Hi ,My name is ${name1}. I want to start free trial now.,My phone number is ${phone1} and email is ${email1}`;
+
+    // const mailtoLink = `mailto:sumiakter6335@gmail.com?subject=${encodeURIComponent(
+    //   emailUseName
+    // )}&body=${encodeURIComponent(emailBody)}`;
 
     // Open the user's default email client
-    window.location.href = mailtoLink;
+    // window.location.href = mailtoLink;
   };
   return (
     <div className="lg:flex justify-center items-center gap-10 my-20 font-semibold">
@@ -35,6 +52,7 @@ const Profitability = () => {
       </div>
       <div className=" lg:h-96  px-6 py-8 ">
         <form
+          ref={form}
           className="grid gap-6 lg:text-start text-center"
           onSubmit={submitEmail}
         >
@@ -42,23 +60,26 @@ const Profitability = () => {
             Start 14-Day Free Trial
           </h1>
           <input
+            required
             type="text"
             placeholder="Name"
-            name="name"
+            name="user_name"
             id="name"
             className="input input-bordered ml-3 rounded-none max-w-sm"
           />
           <input
+            required
             type="text"
             placeholder="Email"
-            name="email"
+            name="user_email"
             id="email"
             className="input input-bordered ml-3 rounded-none max-w-sm"
           />
           <input
+            required
             type="text"
             placeholder="Phone Number"
-            name="phone"
+            name="user_phone"
             id="phone"
             className="input input-bordered ml-3 rounded-none max-w-sm"
           />
